1a9e3b26e099132350015a30d03264387efb2407
Kangmo Kim <kangmo.kim@thankyousoft.com>
2013-08-12 12:59:51 -0700

    Tested Netty&ProtoBuf works well with server and client process.

--- core/gen-proto.sh
+++
@@ -1,1 +0,0 @@
-protoc src/main/java/com/bdtools/doxecute/WorldClockProtocol.proto --java_out=src/main/java
---
+++ core/run-client.sh
@@ -0,0 +1,1 @@
+mvn exec:java -Dexec.mainClass="com.bdtools.doxecute.WorldClockClient" -Dexec.args="localhost 8080 Asia/Seoul"
